a0f33d39ca3b61e0bd1ca8e980f106d5c0978d5a
OlegCoder <hubal.oleg@gmail.com>
2016-03-20 15:04:20 +0200

    Task 1 complete: create 2 fragment and menu item change this frgm

--- app/src/main/java/oleg/hubal/com/tm_homework11/MainActivity.java
+++ app/src/main/java/oleg/hubal/com/tm_homework11/MainActivity.java
@@ -2,12 +2,51 @@ package oleg.hubal.com.tm_homework11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PaintFragment paintFragment;
+    private DragFragment dragFragment;
+    private boolean setFrag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setFragment();
+        return true;
+    }
+
+    private void setFragment() {
+        if(setFrag) {
+            dragFragment = new DragFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frgm_container_AM, dragFragment)
+                    .commit();
+        } else {
+            paintFragment = new PaintFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frgm_container_AM, paintFragment)
+                    .commit();
+        }
+        setFrag = !setFrag;
     }
 }
---
+++ app/src/main/java/oleg/hubal/com/tm_homework11/PaintFragment.java
@@ -0,0 +1,24 @@
+package oleg.hubal.com.tm_homework11;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 20.03.2016.
+ */
+public class PaintFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frgm_paint, container, false);
+
+        return view;
+    }
+}
